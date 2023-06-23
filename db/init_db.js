59d650f5062430577c91ea0170cfb0d5a86cb463
kevin-sperling <ksperling91@gmail.com>
2023-06-22 19:14:46 -0600

    database updates

--- db/init_db.js
+++ db/init_db.js
@@ -8,7 +8,35 @@ async function buildTables() {
   try {
     client.connect();
 
+    await client.query(`
+    DROP TABLES IF EXISTS users;
+    DROP TABLES IF EXISTS games;
+    DROP TABLES IF EXISTS cart;
+    `);
+
+
+
     // drop tables in correct order
+    await client.query(`
+    CREATE TABLE users (
+      id SERIAL PRIMARY KEY,
+      username varchar(255) UNIQUE NOT NULL,
+      password varchar(255) NOT NULL
+    );
+
+    CREATE TABLE games (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) UNIQUE NOT NULL,
+      description TEXT NOT NULL
+    );
+
+    CREATE TABLE cart (
+      id SERIAL PRIMARY KEY,
+      "gameId" INTERGER REFERENCES game(id)
+    );
+
+    
+    `);
 
     // build tables in correct order
   } catch (error) {
