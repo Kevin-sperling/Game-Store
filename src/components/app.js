7eddafeed9240b1ca538bec3717ce3a8d2f30d8d
nazhang8888 <nazhang@pm.me>
2023-07-07 02:56:32 -0400

    renamed according to convention, compiles successfully

--- src/components/app.js
+++ src/components/app.js
@@ -3,13 +3,11 @@ import React, { useState, useEffect } from "react";
 // you can think of that directory as a collection of api adapters
 // where each adapter fetches specific info from our express server's /api route
 
-import { getAPIHealth } from '../axios-services';
-import '../style/App.css';
-import Footer from './footer';
-import HomeContent from './HomeContent';
-import Navbar from './navbar';
-
-
+import { getAPIHealth } from "../axios-services";
+import "../style/App.css";
+import Footer from "./footer";
+import HomeContent from "./homecontent";
+import Navbar from "./navbar";
 
 const App = () => {
   const [APIHealth, setAPIHealth] = useState("");
