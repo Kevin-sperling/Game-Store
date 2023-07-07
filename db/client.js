90779e05e1637810de302136820f3610f8bc7f29
nazhang8888 <nazhang@pm.me>
2023-07-06 21:16:13 -0400

    merge fix

--- db/client.js
+++ db/client.js
@@ -1,11 +1,11 @@
 // Connect to DB
-const { Client } = require('pg');
+const { Client } = require("pg");
 
 // change the DB_NAME string to whatever your group decides on
-const DB_NAME = 'graceshopper';
+const DB_NAME = "graceshopper";
 
-//"host=localhost port=5432 dbname=graceshopper user=postgres 
-const DB_URL = `postgres://postgres:123@localhost:5432/${DB_NAME}`
+//"host=localhost port=5432 dbname=graceshopper user=postgres
+const DB_URL = `postgres://postgres:123@localhost:5432/${DB_NAME}`;
 //  `postgressql://postgres:password@localhost:5432/${DB_NAME}`;
 
 let client = undefined;
@@ -13,23 +13,22 @@ let client = undefined;
 // github actions client config
 if (process.env.CI) {
   client = new Client({
-    host: 'localhost',
-    port: 5433,
-    user: 'zemichael',
-    password: '1238!',
-    database: 'postgres',
+    host: "localhost",
+    port: 5432,
+    user: "postgres",
+    password: "123",
+    database: "postgres",
   });
 } else {
   // local / heroku client config
   client = new Client({
-    host: '127.0.0.1',
-    port: 5433,
-    user: 'zemichael',
-    password: '1238!',
+    host: "127.0.0.1",
+    port: 5432,
+    user: "postgres",
+    password: "123",
     database: DB_NAME,
     ssl: false,
-
-  })
+  });
 }
 
 module.exports = client;
