a36da8144de0d41b86944f55a42e88fcb0d1600a
nazhang8888 <nazhang@pm.me>
2023-07-15 23:19:13 -0400

    added db functions for cart

--- db/models/cart.js
+++ db/models/cart.js
@@ -19,6 +19,37 @@ async function createShoppingCart({ shopperId, orderTotal, quantity }) {
   }
 }
 
+const getShoppingCart = async (cartId) => {
+  try {
+    const { rows: products } = await client.query(`
+      SELECT product_cart.* FROM product_cart 
+      JOIN products ON product_cart."productId" = products.id
+      WHERE product_cart."cartId" = ${cartId}
+      `);
+
+    return products;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getCartByShopperId = async () => {
+  try {
+    const { rows: cart } = await client.query(`
+      SELECT cart."shopperId" as "cartId", product_cart.*
+      FROM product_cart
+      JOIN cart
+      ON product_cart."cartId" = cart."shopperId"
+      `);
+
+    const userCartWithProducts = await attachProductsToProductCart(cart);
+
+    return userCartWithProducts;
+  } catch (error) {
+    throw error;
+  }
+};
+
 async function createGamesCart({ productId, cartId }) {
   try {
     const { rows } = await client.query(
@@ -35,6 +66,88 @@ async function createGamesCart({ productId, cartId }) {
     console.log(err);
   }
 }
+
+const getShoppingCartItemsByUser = async (id) => {
+  try {
+    const {
+      rows: [shopperId],
+    } = await client.query(
+      `
+          SELECT * FROM cart 
+          WHERE "shopperId"=$1;
+      `,
+      [id]
+    );
+
+    if (!shopperId) {
+      return [];
+    }
+
+    return shopperId;
+  } catch (error) {
+    throw error;
+  }
+};
+
+async function updateCart({ id, orderTotal, quantity }) {
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
+  try {
+    if (setString.length < 0) return undefined;
+
+    const {
+      rows: [newShoppingCart],
+    } = await client.query(
+      `
+       UPDATE products
+       SET ${setString}
+       WHERE id=${fields.id}
+       RETURNING *;
+       `,
+      Object.values(fields)
+    );
+    return newShoppingCart;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getCartItemById(id) {
+  try {
+    const {
+      rows: [product],
+    } = await client.query(
+      `
+      SELECT * FROM cart WHERE id = ($1);
+      `,
+      [id]
+    );
+    return product;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+const destroyShoppingCartItem = async (id) => {
+  try {
+    const {
+      rows: [product],
+    } = await client.query(
+      `
+          DELETE FROM cart
+          WHERE "productId"=$1
+          RETURNING *
+          `,
+      [id]
+    );
+
+    return product;
+  } catch (error) {
+    throw error;
+  }
+};
 
 async function addGameToCart({ userId, gameId, quantity }) {
   try {
@@ -127,7 +240,13 @@ async function viewGamesinCart(userId) {
 
 module.exports = {
   createShoppingCart,
+  getShoppingCart,
+  getCartByShopperId,
   createGamesCart,
+  getShoppingCartItemsByUser,
+  updateCart,
+  getCartItemById,
+  destroyShoppingCartItem,
   addGameToCart,
   removeGameFromCart,
   viewGamesinCart,
