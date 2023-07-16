0dad16402b3ea091aae662bff017f9a9e9e35cda
nazhang8888 <nazhang@pm.me>
2023-07-15 23:47:28 -0400

    creating routes for cart

--- api/cart.js
+++ api/cart.js
@@ -1,62 +1,149 @@
 const express = require("express");
 const { requireUser } = require("./utils");
-const {
-  viewCartItems,
-  addGameToCart,
-  removeGameFromCart,
-  viewGamesinCart,
-  increaseQuantity,
-} = require("../db/models/cart");
 const cartRouter = express.Router();
+const {
+  // viewCartItems,
+  // addGameToCart,
+  // removeGameFromCart,
+  // viewGamesinCart,
+  // increaseQuantity,
+  getShoppingCartItemsByUser,
+  getShoppingCart,
+} = require("../db/models/cart");
+const { getGameById } = require("../db");
 
-cartRouter.get("/:userId", async (req, res, next) => {
-  const { userId } = req.params;
+cartRouter.use((req, res, next) => {
+  console.log("A request is being made to /cart");
+  next();
+});
 
+cartRouter.get("/", requireUser, async (req, res, next) => {
   try {
-    const cart = await viewGamesinCart(userId);
-
-    res.send(cart);
+    const user = req.user;
+    const id = user.id;
+    const cart = req.body;
+    console.log("cart", cart);
+    const shoppingCart = await getShoppingCart(id);
+    console.log("shoppingCart", shoppingCart);
+    return res.send(shoppingCart);
   } catch (err) {
     console.log(err);
   }
 });
 
-cartRouter.post("/:userId", async (req, res, next) => {
-  const { userId } = req.params;
-  const { gameId, quantity } = req.body;
-
+cartRouter.post("/", requireUser, async (req, res, next) => {
   try {
-    const addGame = await addGameToCart({ userId, gameId, quantity });
-
-    res.send(addGame);
-  } catch (err) {
-    console.log(err);
+    const shopperId = req.user.id;
+    const { orderTotal, quantity } = req.body;
+    const shoppingCart = await shoppingCart({
+      shopperId,
+      orderTotal,
+      quantity,
+    });
+    console.log(shoppingCart);
+    return res.send(shoppingCart);
+  } catch (error) {
+    return next(error);
   }
 });
 
-cartRouter.post("/:userId/:gameTitle", async (req, res, next) => {
-  const { userId, gameTitle } = req.params;
-  // const { gameId, quantity } = req.body
-
+cartRouter.patch("/:shoppingCartId", requireUser, async (req, res, next) => {
   try {
-    const addGame = await increaseQuantity({ gameTitle });
+    const { gameId } = req.params;
+    const { title, genre, release_date, price, image_path, platform } =
+      req.body;
+    const game = await getShoppingCartItemsByUser(gameId);
 
-    res.send(addGame);
-  } catch (err) {
-    console.log(err);
+    if (req.user.id !== game.userId) {
+      return next({
+        name: "UnauthorizedUserError",
+        message: "You cannot update a game that is not in your cart",
+      });
+    }
+
+    const updateCart = await updateCart({
+      id: productId,
+      title,
+      genre,
+      release_date,
+      price,
+      image_path,
+      platform,
+    });
+    return res.send(updateCart);
+  } catch (error) {
+    return next(error);
   }
 });
 
-cartRouter.delete("/:userId/:cartId", async (req, res, next) => {
-  const { userId, cartId } = req.params;
-
+cartRouter.delete("/:shoppingCartId", requireUser, async (req, res, next) => {
   try {
-    const removeGame = await removeGameFromCart({ userId, cartId });
+    const { gameId } = req.params;
+    const game = await getGameById(gameId);
 
-    res.send(removeGame);
-  } catch (err) {
-    console.log(err);
+    if (req.user.id !== game.userId) {
+      return next({
+        name: "UnauthorizedUserError",
+        message: "You cannot delete a game that is not in your cart",
+      });
+    }
+
+    const destroyShoppingCartItem = await destroyShoppingCartItem(gameId);
+    game.success = true;
+    return res.send(shoppingCart);
+  } catch (error) {
+    return next(error);
   }
 });
+
+// cartRouter.get("/:userId", async (req, res, next) => {
+//   const { userId } = req.params;
+
+//   try {
+//     const cart = await viewGamesinCart(userId);
+
+//     res.send(cart);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+
+// cartRouter.post("/:userId", async (req, res, next) => {
+//   const { userId } = req.params;
+//   const { gameId, quantity } = req.body;
+
+//   try {
+//     const addGame = await addGameToCart({ userId, gameId, quantity });
+
+//     res.send(addGame);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+
+// cartRouter.post("/:userId/:gameTitle", async (req, res, next) => {
+//   const { userId, gameTitle } = req.params;
+//   // const { gameId, quantity } = req.body
+
+//   try {
+//     const addGame = await increaseQuantity({ gameTitle });
+
+//     res.send(addGame);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+
+// cartRouter.delete("/:userId/:cartId", async (req, res, next) => {
+//   const { userId, cartId } = req.params;
+
+//   try {
+//     const removeGame = await removeGameFromCart({ userId, cartId });
+
+//     res.send(removeGame);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
 
 module.exports = cartRouter;
